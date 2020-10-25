1d0275151d37e962a1557f1fe0f42921bdddc7de
j.parker <me@jasonparker.info>
2020-10-25 02:06:23 -0400

    add decode functionality

--- src/main/java/info/jasonparker/string/utility/UIController.java
+++ src/main/java/info/jasonparker/string/utility/UIController.java
@@ -11,19 +11,41 @@ import java.util.Base64;
 public class UIController {
 
     @FXML
-    private TextArea input;
+    private TextArea encode;
 
     @FXML
-    private TextArea output;
+    private TextArea decode;
 
     @FXML
     private AnchorPane pane;
 
     @FXML
     void toBase64(Event event) {
-        var input = this.input.getText();
+        var input = this.encode.getText();
         var base64 = Base64.getEncoder().encodeToString(input.getBytes(StandardCharsets.UTF_8));
-        output.setText(base64);
+        decode.setText(base64);
+    }
+
+    @FXML
+    void toString(Event event) {
+        try {
+            var output = this.decode.getText();
+            var string = Base64.getDecoder().decode(output.getBytes(StandardCharsets.UTF_8));
+            this.encode.setText(new String(string));
+        } catch (IllegalArgumentException e) {
+            this.encode.setText("Invalid Base64");
+        }
+    }
+
+    @FXML
+    void clear(Event event) {
+        var source = ((TextArea) event.getSource()).getId();
+
+        if (source.compareToIgnoreCase("output") < 0) {
+            this.encode.clear();
+        }
+
+        this.decode.clear();
     }
 }
 
